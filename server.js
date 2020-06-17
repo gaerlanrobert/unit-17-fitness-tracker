d7f2501d00c19e8d8f84484d3bfc31304617f374
Robert Gaerlan <gaerlanrobert@gmail.com>
2020-06-16 19:24:04 -0700

    Changes to homework 17 - fitness tracker

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
